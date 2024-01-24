8784988eec2e6a9b3c8ae9ef192acc281c834093
Omar-ASUS <eaomarb@gmail.com>
2024-01-24 20:28:52 +0100

    update

---
+++ src/UF2/Activitats/A1/A1_4.java
@@ -0,0 +1,8 @@
+//Calculadora: Fer un programa principal que contingui un menú amb 5 opcions:
+// Sumar, restar, multiplicar, dividir, sortir
+
+public class A1_4 {
+    public static void main(String[] args) {
+
+    }
+}
