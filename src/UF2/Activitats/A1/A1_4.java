d61d992a6bd4e69a6ce1987844a9c3b23e422dd5
Omar-MSI <eaomarb@gmail.com>
2024-01-26 21:26:19 +0100

    update

--- src/UF2/Activitats/A1/A1_4.java
+++ src/UF2/Activitats/A1/A1_4.java
@@ -1,27 +1,20 @@
 //Calculadora: Fer un programa principal que contingui un menú amb 5 opcions:
 // Sumar, restar, multiplicar, dividir, sortir
 
+//te pide n1 y n2 despues de haber introducido mal la opcion, cuando la vuelves introduces bien se acaba el programa
+
 import java.util.Scanner;
 
 public class A1_4 {
 
     public static void menuPrincipal() {
-        Scanner teclat = new Scanner(System.in);
-
         System.out.println("1. Sumar");
         System.out.println("2. Restar");
         System.out.println("3. Multiplicar");
         System.out.println("4. Dividir");
         System.out.println("5. Sortir");
-
-        System.out.println("Escull una opció (1-5)");
-        int opcio = teclat.nextInt();
-
-        System.out.println("Introdueix el primer número: ");
-        int n1 = teclat.nextInt();
-        System.out.println("Introdueix el segon número: ");
-        int n2 = teclat.nextInt();
     }
+
 
     public static int sumar(int n1, int n2) {
         return n1 + n2;
@@ -40,7 +33,7 @@ public class A1_4 {
     }
 
     public static void sortir() {
-        System.out.println("Has sortit del programa.");
+        System.out.println("Has sortit del programa, fins aviat!");
     }
 
 
@@ -48,39 +41,42 @@ public class A1_4 {
         Scanner teclat = new Scanner(System.in);
 
         menuPrincipal();
-
         System.out.println("Escull una opció (1-5)");
         int opcio = teclat.nextInt();
 
-        System.out.println("Introdueix el primer número: ");
-        int n1 = teclat.nextInt();
-        System.out.println("Introdueix el segon número: ");
-        int n2 = teclat.nextInt();
+        do {
+            System.out.println("Introdueix el primer número: ");
+            int n1 = teclat.nextInt();
+            System.out.println("Introdueix el segon número: ");
+            int n2 = teclat.nextInt();
+            switch (opcio) {
+                case 1:
+                    System.out.println(sumar(n1, n2));
 
-        switch (opcio) {
-            case 1:
-                System.out.println(sumar(n1, n2));
-                menuPrincipal();
-                break;
-            case 2:
-                System.out.println(restar(n1, n2));
-                menuPrincipal();
-                break;
-            case 3:
-                System.out.println(multiplicar(n1, n2));
-                menuPrincipal();
-                break;
-            case 4:
-                System.out.println(dividir(n1, n2));
-                menuPrincipal();
-                break;
-            case 5:
-                sortir();
-                break;
-            default:
-                System.out.println("Revisa bé la opció que has escollit (1-5)!");
-                menuPrincipal();
-        }
+                    menuPrincipal();
+                    break;
+                case 2:
+                    System.out.println(restar(n1, n2));
+                    menuPrincipal();
+                    break;
+                case 3:
+                    System.out.println(multiplicar(n1, n2));
+                    menuPrincipal();
+                    break;
+                case 4:
+                    System.out.println(dividir(n1, n2));
+                    menuPrincipal();
+                    break;
+                case 5:
+                    sortir();
+                    break;
+                default:
+                    System.out.println("Revisa bé la opció que has escollit (1-5)!");
+                    menuPrincipal();
+            }
+            System.out.println("Escull una opció (1-5)");
+            opcio = teclat.nextInt();
+        } while (opcio == 1 || opcio == 2 || opcio == 3 || opcio == 4 || opcio == 5);
 
     }
 }
