549b9d699f2d68e1c9a2990a87625e3c9651184c
Omar-ASUS <eaomarb@gmail.com>
2024-02-06 19:53:03 +0100

    update

--- src/UF2/Activitats/A2/A2_1.java
+++ src/UF2/Activitats/A2/A2_1.java
@@ -1,16 +1,21 @@
 public class A2_1 {
     public static double percentatgePlacesLliuresFila(boolean[][] parking, int fila) {
         int placesLliures = 0;
-        for (int columna = 0; fila < parking[fila].length; columna++) {
-            if (parking[fila][columna]) {
+        for (int columna = 0; columna < parking[fila].length; columna++) {
+            if (!parking[fila][columna]) {
                 placesLliures++;
             }
         }
+        System.out.println(placesLliures);
         return ((double) placesLliures / parking[fila].length) * 100;
     }
 
-    public static double percentatgePlacesLliuresParking(boolean[][] parking, int fila){
-        int placesLliures
+    public static double percentatgePlacesLliuresParking(boolean[][] parking) {
+        double placesLliures = 0;
+        for (int fila = 0; fila < parking.length; fila++) {
+            placesLliures += percentatgePlacesLliuresFila(parking, fila);
+        }
+        return placesLliures / parking.length;
     }
 
     public static void main(String[] args) {
@@ -19,6 +24,7 @@ public class A2_1 {
                         {false, true, true, true, true},
                         {true, false, false, true, false},
                         {false, true, true, false, true}};
+        System.out.println(percentatgePlacesLliuresParking(parking));
 
     }
 }
