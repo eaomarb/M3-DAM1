0c26cdd4076caa611d0aff00a833c76857b44e81
Omar-ASUS <eaomarb@gmail.com>
2024-02-15 18:02:20 +0100

    update

--- src/UF2/Activitats/A2/A2_3.java
+++ src/UF2/Activitats/A2/A2_3.java
@@ -1,11 +1,14 @@
 public class A2_3 {
     public static int calculateNeighborsSum(int[][] matrix, int row, int col) {
         int neigboursSum = 0;
+        int nPosition = matrix[row][col]; //restar al final
 
-        for (row = row - 1; row < row + 1; row++) {
-            for (col = col - 1; col < col + 1; col++) {
-                if (matrix[row][col]){
-
+        for (row = 0; row < matrix.length; row++){
+            for (col = 0; col < matrix[row].length; col++){
+                if (row == 0 || col != 0){
+                    System.out.println(matrix[row][col-1]);
+                } else if (row != 0 || col == 0){
+                    System.out.println(matrix[row-1][col]);
                 }
             }
         }
