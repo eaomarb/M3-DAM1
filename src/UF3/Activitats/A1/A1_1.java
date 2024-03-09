7c412976ceed5f91bea648821fb07ca4abbed03c
Omar-ASUS <eaomarb@gmail.com>
2024-03-09 21:36:14 +0100

    update

---
+++ src/UF3/Activitats/A1/A1_1.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class A1_1 {
+    public static void main(String[] args)throws FileNotFoundException {
+
+    }
+}
