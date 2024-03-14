6a7bd16816f791b3af1c9d60d8bcbefcabebf9fa
Omar-ASUS <eaomarb@gmail.com>
2024-03-14 18:48:14 +0100

    update

--- src/UF3/Activitats/A1/A1_3.java
+++ src/UF3/Activitats/A1/A1_3.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.PrintWriter;
+
 public class A1_3 {
-    public static void main(String[] args) {
-        
+    public static void toUpperAndLowerCase(String fileName) throws FileNotFoundException {
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
     }
 }
