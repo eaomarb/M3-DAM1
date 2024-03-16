a3cb062458c5c75613523bce381f5cbfb25fa2dc
omar <eaomarb@gmail.com>
2024-03-16 19:51:20 +0100

    commit

--- src/UF3/Activitats/A1/A1_3.java
+++ src/UF3/Activitats/A1/A1_3.java
@@ -4,11 +4,34 @@ import java.io.File;
 import java.io.PrintWriter;
 
 public class A1_3 {
-    public static void toUpperAndLowerCase(String fileName) throws FileNotFoundException {
+    public static void swapCase() throws FileNotFoundException {
+        //Llegim el fitxer i el guardem com a inputFile amb el Scanner
+        Scanner inputFile = new Scanner(new File("src/UF3/Activitats/A1/A1_3.txt"));
 
+        while (inputFile.hasNextLine()) {
+            String str = inputFile.nextLine();
+            char[] strToChar = str.toCharArray();
+            char c;
+
+            for (int i = 0; i < strToChar.length; i++) {
+                c = strToChar[i];
+                if (Character.isLowerCase(c)) {
+                    strToChar[i] = Character.toUpperCase(c);
+                } else {
+                    strToChar[i] = Character.toLowerCase(c);
+                }
+            }
+
+            String string = new String(strToChar);
+            System.out.println(string);
+
+        }
+
+
+        inputFile.close();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-
+        swapCase();
     }
 }
