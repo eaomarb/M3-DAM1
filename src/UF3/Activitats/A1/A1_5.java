bc14023bfdc8b9633cad1e4cc313b4e0b57ebb3b
omar <eaomarb@gmail.com>
2024-03-21 14:47:01 +0100

    UF3/A1_5

--- src/UF3/Activitats/A1/A1_5.java
+++ src/UF3/Activitats/A1/A1_5.java
@@ -1,30 +1,53 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class A1_5 {
-    public static void writeToFile(String day, String month, String year, String description) throws IOException {
-        FileWriter file = new FileWriter("src/UF3/Activitats/A1/A1_5.txt", true);
+    public static void addDate(String day, String month, String year, String description) throws IOException {
+        File file = new File("src/UF3/Activitats/A1/A1_5.txt");
+        FileWriter fileWriter = new FileWriter(file, true);
 
-        file.write(day + "/" + month + "/" + year + " " + description + "\n");
-        file.close();
+        if (file.length() != 0) {
+            fileWriter.write("\n" + day + "/" + month + "/" + year + " " + description);
+        } else {
+            fileWriter.write(day + "/" + month + "/" + year + " " + description);
+        }
+
+        fileWriter.close();
     }
+
+    public static int findLineByDate(String day, String month, String year) throws IOException {
+        File file = new File("src/UF3/Activitats/A1/A1_5.txt");
+        Scanner scFile = new Scanner(file);
+
+        boolean lineFound = false;
+        int = file.
+
+        while (!lineFound) {
+            if (scFile.next().equalsIgnoreCase(day)){
+
+            }
+        }
+        return 0;
+    }
+
 
     public static void main(String[] args) throws IOException {
         Scanner kb = new Scanner(System.in);
 
         System.out.println("Enter the day: ");
-        String day = kb.next();
+        String day = kb.nextLine();
 
         System.out.println("Enter the month: ");
-        String month = kb.next();
+        String month = kb.nextLine();
 
         System.out.println("Enter the year: ");
-        String year = kb.next();
+        String year = kb.nextLine();
 
         System.out.println("Enter the description: ");
-        String description = kb.next();
+        String description = kb.nextLine();
 
-        writeToFile(day, month, year, description);
+        addDate(day, month, year, description);
     }
 }
