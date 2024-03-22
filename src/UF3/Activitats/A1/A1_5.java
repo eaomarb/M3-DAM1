30f04c7bdfa4ef9a0c8fd1edeef6a14f966f3234
omar <eaomarb@gmail.com>
2024-03-22 06:30:17 +0100

    UF3/A1_5

--- src/UF3/Activitats/A1/A1_5.java
+++ src/UF3/Activitats/A1/A1_5.java
@@ -4,51 +4,58 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class A1_5 {
-    public static void addDate(String day, String month, String year, String description) throws IOException {
+    public static void addDate(int day, int month, int year, String description) throws Exception {
         File file = new File("src/UF3/Activitats/A1/A1_5.txt");
         FileWriter fileWriter = new FileWriter(file, true);
 
         if (file.length() != 0) {
-            fileWriter.write("\n" + day + "/" + month + "/" + year + " " + description);
+            fileWriter.write("\n" + day + " " + month + " " + year + " " + description);
         } else {
-            fileWriter.write(day + "/" + month + "/" + year + " " + description);
+            fileWriter.write(day + " " + month + " " + year + " " + description);
         }
 
         fileWriter.close();
     }
 
-    public static int findLineByDate(String day, String month, String year) throws IOException {
+    public static int findLineByDate(int day, int month, int year) throws Exception {
         File file = new File("src/UF3/Activitats/A1/A1_5.txt");
         Scanner scFile = new Scanner(file);
 
-        boolean lineFound = false;
-        int =file.
-
-        while (!lineFound) {
-            if (scFile.next().equalsIgnoreCase(day)) {
-
+        int line = 1;
+        scFile.nextLine();
+        while (scFile.hasNextLine()) {
+            if (scFile.nextInt() == day && scFile.nextInt() == month && scFile.nextInt() == year) {
+                return line;
             }
+
+            line++;
         }
-        return 0;
+
+        return -1;
     }
 
 
-    public static void main(String[] args) throws IOException {
-        /* TODO: Parse string to int */
+    public static void main(String[] args) throws Exception {
         Scanner kb = new Scanner(System.in);
 
         System.out.println("Enter the day: ");
-        String day = kb.nextLine();
-        
+        String strDay = kb.nextLine();
+
         System.out.println("Enter the month: ");
-        String month = kb.nextLine();
+        String strMonth = kb.nextLine();
 
         System.out.println("Enter the year: ");
-        String year = kb.nextLine();
+        String strYear = kb.nextLine();
 
         System.out.println("Enter the description: ");
         String description = kb.nextLine();
 
+        int day = Integer.parseInt(strDay);
+        int month = Integer.parseInt(strMonth);
+        int year = Integer.parseInt(strYear);
+
         addDate(day, month, year, description);
+
+        System.out.println(findLineByDate(2, 2, 2));
     }
 }
