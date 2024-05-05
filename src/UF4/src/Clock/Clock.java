7c1eab03a91dbca59d4156fca66bc20c6a17e866
eaomarb <150187124+eaomarb@users.noreply.github.com>
2024-05-05 22:24:12 +0200

    Clock

---
+++ src/UF4/src/Clock/Clock.java
@@ -0,0 +1,35 @@
+package Clock;
+
+public class Clock {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+public Clock(){
+    
+}
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+}
