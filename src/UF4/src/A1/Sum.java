171e39a0283ae9a800d40c8964e67c6058d9b2ee
omar <eaomarb@gmail.com>
2024-05-06 14:45:03 +0200

    sum

--- src/UF4/src/A1/Sum.java
+++ src/UF4/src/A1/Sum.java
@@ -1,7 +1,25 @@
 package A1;
 
-public class Sum {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Sum {
+    public static int sumFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        int sum = 0;
+
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "src/UF4/src/A1/sum.txt";
+        System.out.println(sumFile(fileName));
     }
 }
